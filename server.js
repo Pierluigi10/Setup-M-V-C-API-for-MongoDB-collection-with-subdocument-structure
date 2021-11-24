4f738d8f94f116502dc9a7bee96152e5482a96a4
pierluigi <pierluigibaiano@gmail.com>
2021-11-24 13:46:52 +0100

    build CRUD functionality DELETE + test

--- server.js
+++ server.js
@@ -35,6 +35,15 @@ app.patch('/simpleUsers/update/:id', async (req, res) => {
     });
 });
 
+// simple users: DELETE
+app.delete('/simpleUsers/delete/:id', async (req, res) => {
+    const id = req.params.id;
+    const result = await SimpleUsersController.deleteSimpleUser(id);
+    res.json({
+        result
+    });
+});
+
 app.listen(port, () => {
   console.log(`API is now listening on port http://localhost:${port}`);
 });
