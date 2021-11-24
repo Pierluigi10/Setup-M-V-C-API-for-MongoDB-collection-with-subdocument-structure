2a82de3f8bdf49a0d37fa9b587c1256a809b2ddd
pierluigi <pierluigibaiano@gmail.com>
2021-11-24 11:41:12 +0100

    bugs fixed and add a test (rest client)

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from "express";
-// import * as SimpleUsersController from "./controllers/simpleUsersController.js";
+import * as SimpleUsersController from "./controllers/simpleUsersController.js";
 import mongoose from "mongoose";
 
 mongoose.connect("mongodb://localhost:27017/test");
