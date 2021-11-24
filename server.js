0a1c6c2b3876f4b51f244aaedce192ae7dcc1781
pierluigi <pierluigibaiano@gmail.com>
2021-11-24 14:56:32 +0100

    build CRUD UPDATE (PATCH) functionality for subdocuments

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import * as SimpleUsersController from "./controllers/simpleUsersController.js";
-import * as NestedUsersController from './controllers/nestedUsersController.js';
+import * as NestedUsersController from "./controllers/nestedUsersController.js";
 import mongoose from "mongoose";
 
 mongoose.connect("mongodb://localhost:27017/test");
@@ -18,59 +18,68 @@ app.get("/simpleUsers", async (req, res) => {
 });
 
 // simple users: CREATE
-app.post('/simpleUsers/create', async (req, res) => {
-    const simpleUserObj = req.body;
-    const result = await SimpleUsersController.createSimpleUser(simpleUserObj);
-    res.json({
-        result
-    });
+app.post("/simpleUsers/create", async (req, res) => {
+  const simpleUserObj = req.body;
+  const result = await SimpleUsersController.createSimpleUser(simpleUserObj);
+  res.json({
+    result,
+  });
 });
 
 // simple users: UPDATE
-app.patch('/simpleUsers/update/:id', async (req, res) => {
-    const id = req.params.id;
-    const updateFields = req.body
-    const result = await SimpleUsersController.updateSimpleUser(id, updateFields);
-    res.json({
-        result
-    });
+app.patch("/simpleUsers/update/:id", async (req, res) => {
+  const id = req.params.id;
+  const updateFields = req.body;
+  const result = await SimpleUsersController.updateSimpleUser(id, updateFields);
+  res.json({
+    result,
+  });
 });
 
 // simple users: DELETE
-app.delete('/simpleUsers/delete/:id', async (req, res) => {
-    const id = req.params.id;
-    const result = await SimpleUsersController.deleteSimpleUser(id);
-    res.json({
-        result
-    });
+app.delete("/simpleUsers/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  const result = await SimpleUsersController.deleteSimpleUser(id);
+  res.json({
+    result,
+  });
 });
 
 //NESTED users
 
-// nested users: READ all 
-app.get('/nestedUsers', async (req, res) => {
-    res.json({
-        nestedUsers: await NestedUsersController.getAllNestedUsers()
-    });
+// nested users: READ all
+app.get("/nestedUsers", async (req, res) => {
+  res.json({
+    nestedUsers: await NestedUsersController.getAllNestedUsers(),
+  });
 });
 
 // nested users: READ all emails
-app.get('/nestedUsers/emails', async (req, res) => {
-    res.json({
-        nestedUsers: await NestedUsersController.getAllNestedUsersEmails()
-    });
+app.get("/nestedUsers/emails", async (req, res) => {
+  res.json({
+    nestedUsers: await NestedUsersController.getAllNestedUsersEmails(),
+  });
 });
 
 // nested users: CREATE
-app.post('/nestedUsers/create', async (req, res) => {
-    const nestedUserObj = req.body;
-    await NestedUsersController.createNestedUser(nestedUserObj, (result) => {
-        res.json({
-            result
-        });
+app.post("/nestedUsers/create", async (req, res) => {
+  const nestedUserObj = req.body;
+  await NestedUsersController.createNestedUser(nestedUserObj, (result) => {
+    res.json({
+      result,
     });
+  });
 });
 
+// nested users: UPDATE
+app.patch("/nestedUsers/update/:id", async (req, res) => {
+  const id = req.params.id;
+  const updateFields = req.body;
+  const result = await NestedUsersController.updateNestedUser(id, updateFields);
+  res.json({
+    result,
+  });
+});
 
 app.listen(port, () => {
   console.log(`API is now listening on port http://localhost:${port}`);
