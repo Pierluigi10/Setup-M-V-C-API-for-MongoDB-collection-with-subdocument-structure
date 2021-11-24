83ba6fe2661c46a1313f390f76cd87957eec1f84
pierluigi <pierluigibaiano@gmail.com>
2021-11-24 11:35:49 +0100

    build CRUD functionality READ

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-import express from 'express';
-import * as SimpleUsersController from './controllers/simpleUsersController.js';
-import mongoose from 'mongoose';
- 
-mongoose.connect('mongodb://localhost:27017/test');
- 
+import express from "express";
+// import * as SimpleUsersController from "./controllers/simpleUsersController.js";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/test");
+
 const app = express();
-const port = 3033;
+const port = 3035;
 
 app.use(express.json());
 
+// simple users: READ
+app.get("/simpleUsers", async (req, res) => {
+  res.json({
+    simpleUsers: await SimpleUsersController.getAllSimpleUsers(),
+  });
+});
 
 app.listen(port, () => {
-    console.log(`API is now listening on port ${port}`);
+  console.log(`API is now listening on port http://localhost:${port}`);
 });
